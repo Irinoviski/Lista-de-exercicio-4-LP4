cb35a8219db5edd7d18670bef661122ac31b5d82
Irinoviski <110059431+Irinoviski@users.noreply.github.com>
2024-12-09 15:28:01 -0400

    Atividade 4 de LP4.js

--- Atividade 4 de LP4.js
+++ Atividade 4 de LP4.js
@@ -9,7 +9,7 @@ const port = 5000;
 // configuraçao do server
 const server = https.createServer((req, res) => {
 
-  //url que vai ser inserida pelo usuario
+  //url pro usuário poder acessar as páginas
   let url = req.url;
   if (url == '/') {
 
@@ -23,11 +23,14 @@ const server = https.createServer((req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 
-    // página professores
+    // página veículos
+    //eu esqueci como que faz pra fazer o GET, POST, PUT e DELETE
     res.write('<h4>você está na página veiculos</h4>');
-    res.write('<img>imagem do celta preto</img>');
+    res.write('<img>http://img.revendapro.com.br/15ea7addbf5c9d9028af7ab2e261efa6_thumb.jpeg</img>');
     res.write('<p>carro: celta, fabricante: eu nao sei qual empresa faz o celta, ano: 2000, modelo: aaa, combustível: gasolina, cor: preto, preco: R$ 20.000,00</p>');
+    res.write('<img>https://gazetanortesc.com.br/wp-content/uploads/2022/09/foto-1-6-1.jpg</img>');
     res.write('<p>carro: fiat uno, fabricante: Fiat, ano: 2005, modelo: uno, combustivel: gasolina, cor: branco, preco: R$ 10.000,00</p>');
+    res.write('<img>http://topcarspecs.com/manufacturers/ferrari/ferrari-veneno/ferrari-veneno-4.jpg</img>');
     res.write('carro: ferrari, fabricante: Ferrari, ano: 2013, modelo: venom, combustível: gasolina aditivada (para melhor desempenho), cor: vermelho, preco: R$ 3.000.000,01')
 
     res.end();
